456d459b5c868d7408a7c11bf7264f49ce879ca3
Dan Harling <dharling101@gmail.com>
2023-06-14 16:09:09 +0100

    chore: add types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import express from 'express';
+import {express, Request, Response}  from 'express';
+import path from 'path';
 
 const app = express();
 
-const updateRecEngine = (timeSpent: Number, article: String, user: String): void => {
+const updateRecEngine = (timeSpent: Number, article: string, user: string): void => {
 	// update recommendation engine
 }
 
-const getSummary = (article: string) => {
+const chatgpt = (prompt: string): string => {
+	return "This is a summary of the article";
+}
+
+const getSummary = (article: string):any => {
 	// query chatgpt for summary
 	let prompt = "Summarize this article: " + article;
 	let summary = chatgpt(prompt);
@@ -20,12 +25,12 @@ const getArticles = (numRequired: Number, user: String): String => {
 }
 
 interface Summary {
-	image: ImageBitmap;
+	image: string;
 	headline: string;
 	summary: string;
 }
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
 	// user hits this base endpoint and gets directed to the first page
 	// we send the initial article summaries, images and headlines
 	// how many articles do we send?
@@ -37,10 +42,11 @@ app.get('/', (req, res) => {
 		summaries.push(getSummary(article));
 	}
 
-	res.send(articles);
+	// res.send(articles);
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/swipe', (req, res) => {
+app.get('/swipe', (req:Request, res: Request) => {
 	// on swipe this endpoint is called from an ajax post request
 	// this sends data about the length of time the user has spent on that article
 	// this data is used to update the recommendation engine
